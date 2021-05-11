e34ec191b5b531fbdf731fa4a5a23789fd343a44
nzoniD <56226046+nzoniD@users.noreply.github.com>
2021-05-11 12:20:13 +0200

    Minor bug fixes

--- app/src/main/java/it/unipi/dii/ntc/Add_devices.java
+++ app/src/main/java/it/unipi/dii/ntc/Add_devices.java
@@ -1,15 +1,20 @@
 package it.unipi.dii.ntc;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TableLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Add_devices extends AppCompatActivity
@@ -17,8 +22,10 @@ public class Add_devices extends AppCompatActivity
 
 	private static final String TAG = Add_devices.class.getName();
 	private static final int REQUEST_ENABLE_BT = 1234;
+	private static final int ACCESS_FINE_LOCATION_STATE_PERMISSION_CODE = 1024;
 	private BroadcastBLTReceiver BLTReceiver;
 
+	@RequiresApi(api = Build.VERSION_CODES.M)
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -31,8 +38,9 @@ public class Add_devices extends AppCompatActivity
 			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}
+		checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, ACCESS_FINE_LOCATION_STATE_PERMISSION_CODE);
 		if (!bluetoothAdapter.isDiscovering()) {
-			bluetoothAdapter.startDiscovery();
+				Log.i(TAG, "onCreate: isDiscovering " + bluetoothAdapter.startDiscovery());
 		}
 		IntentFilter BLTIntFilter = new IntentFilter();
 		BLTIntFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
@@ -42,6 +50,8 @@ public class Add_devices extends AppCompatActivity
 		BLTReceiver = new BroadcastBLTReceiver(tView);
 		getApplicationContext().registerReceiver(BLTReceiver, BLTIntFilter);
 	}
+
+
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
@@ -61,4 +71,27 @@ public class Add_devices extends AppCompatActivity
 		super.onDestroy();
 		getApplicationContext().unregisterReceiver(BLTReceiver);
 	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+	{
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		String permissionName = String.valueOf(requestCode);
+		if (requestCode == ACCESS_FINE_LOCATION_STATE_PERMISSION_CODE)
+			permissionName = "ACCESS_FINE_LOCATION_STATE";
+		boolean granted = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+		Log.d(TAG, permissionName + " " + (granted ? "granted." : "denied."));
+	}
+
+	@RequiresApi(api = Build.VERSION_CODES.M)
+	private void checkPermission(String permission, int requestCode)
+	{
+		if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+			Log.d(TAG, "Permission " + permission + " already granted.");
+			return;
+		}
+		requestPermissions(new String[] { permission }, requestCode);
+	}
+	
 }
