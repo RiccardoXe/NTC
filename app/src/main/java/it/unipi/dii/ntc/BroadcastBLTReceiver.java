460b67c3eb17665e4ece6e76c5414093d833437e
Niccolò Scatena <speedjack95@gmail.com>
2021-05-11 11:28:24 +0200

    Code style

--- app/src/main/java/it/unipi/dii/ntc/BroadcastBLTReceiver.java
+++ app/src/main/java/it/unipi/dii/ntc/BroadcastBLTReceiver.java
@@ -13,46 +13,48 @@ import android.widget.TextView;
 import java.util.HashMap;
 import java.util.Set;
 
-public class BroadcastBLTReceiver extends BroadcastReceiver {
+public class BroadcastBLTReceiver extends BroadcastReceiver
+{
 
-    private HashMap<String,String> discoveredBLT= new HashMap<>();
-    private TableLayout BLTTable;
-    private static final String TAG = MainActivity.class.getName();
+	private static final String TAG = MainActivity.class.getName();
+	private final HashMap<String, String> discoveredBLT = new HashMap<>();
+	private final TableLayout BLTTable;
 
-    public BroadcastBLTReceiver(TableLayout BLTTable) {
-        this.BLTTable = BLTTable;
-    }
+	public BroadcastBLTReceiver(TableLayout BLTTable)
+	{
+		this.BLTTable = BLTTable;
+	}
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if (BluetoothDevice.ACTION_NAME_CHANGED.equals(action)) {
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		String action = intent.getAction();
+		if (BluetoothDevice.ACTION_NAME_CHANGED.equals(action)) {
 
-            // Discovery has found a device. Get the BluetoothDevice
-            // object and its info from the Intent.
-            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-            String deviceName = device.getName();
-            String deviceHardwareAddress = device.getAddress(); // MAC address
-            Log.i(TAG, "onReceive: Trovato dispositivo " + deviceName);
-            Log.i(TAG, "onReceive: Trovato dispositivo " + deviceHardwareAddress);
-            discoveredBLT.put(deviceHardwareAddress, deviceName);
-        }
-        else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
-            //Update grafic interface
+			// Discovery has found a device. Get the BluetoothDevice
+			// object and its info from the Intent.
+			BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+			String deviceName = device.getName();
+			String deviceHardwareAddress = device.getAddress(); // MAC address
+			Log.i(TAG, "onReceive: Trovato dispositivo " + deviceName);
+			Log.i(TAG, "onReceive: Trovato dispositivo " + deviceHardwareAddress);
+			discoveredBLT.put(deviceHardwareAddress, deviceName);
+		} else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+			//Update grafic interface
 
-            Set<String> keys = discoveredBLT.keySet();
-            for (String key: keys){
-                TableRow row = new TableRow(context);
-                TextView tx = new TextView(context);
-                String tmp = discoveredBLT.get(key);
-                if (tmp.equals(null)){
-                    tx.setText(key);
-                }else{
-                    tx.setText(tmp);
-                }
-                row.addView(tx);
-                BLTTable.addView(row);
-            }
-        }
-    }
+			Set<String> keys = discoveredBLT.keySet();
+			for (String key : keys) {
+				TableRow row = new TableRow(context);
+				TextView tx = new TextView(context);
+				String tmp = discoveredBLT.get(key);
+				if (tmp.equals(null)) {
+					tx.setText(key);
+				} else {
+					tx.setText(tmp);
+				}
+				row.addView(tx);
+				BLTTable.addView(row);
+			}
+		}
+	}
 }
